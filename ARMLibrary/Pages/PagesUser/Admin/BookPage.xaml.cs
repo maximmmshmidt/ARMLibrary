b58d1df6ac029513ce18fb514078edfe716ea1d2
maximmmshmidt <maximmm.shmidt@mail.ru>
2023-03-23 23:14:56 +0500

    asd

--- ARMLibrary/Pages/PagesUser/Admin/BookPage.xaml.cs
+++ ARMLibrary/Pages/PagesUser/Admin/BookPage.xaml.cs
@@ -2,17 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace ARMLibrary.Pages.PagesUser.Admin
 {
@@ -31,10 +24,6 @@ namespace ARMLibrary.Pages.PagesUser.Admin
         public BookPage()
         {
             InitializeComponent();
-            List<string> sortTypeList = new List<string>()
-            {
-                "наименование", "остаток на складе", "стоимость"
-            };
             productTypes = new List<Book>
             {
                 new Book()
