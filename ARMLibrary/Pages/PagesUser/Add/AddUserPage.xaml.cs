9835877abe658df31b6dce047c6538825c198a6e
maximmmshmidt <maximmm.shmidt@mail.ru>
2023-04-07 22:12:31 +0500

    Добавление Пользователя

--- ARMLibrary/Pages/PagesUser/Add/AddUserPage.xaml.cs
+++ ARMLibrary/Pages/PagesUser/Add/AddUserPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ARMLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,58 @@ namespace ARMLibrary.Pages.PagesUser.Add
     /// </summary>
     public partial class AddUserPage : Page
     {
+        readonly Core db = new Core();
+        public int usAddIdViewUser = 0;
         public AddUserPage()
         {
             InitializeComponent();
+            if (App.loginAuntificate.idViewUser == 1)
+            {
+                VisibleVid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                usAddIdViewUser = 3;
+            }
+        }
+
+        private void AddUserLibrary(object sender, RoutedEventArgs e)
+        {
+            if (NumberPhoneTB != null && WorkTB!= null && AdresTB != null && YearBitrhtTB != null && PatronicTB!= null && LastNameTB!= null && FirstNameTB != null && PasswordTB != null && LoginTB != null)
+            {
+                if (true)
+                {
+                    User user = new User()
+                    {
+                        LastName = LastNameTB.Text,
+                        FirstName = FirstNameTB.Text,
+                        Patronymic = PatronicTB.Text,
+                        Password = PasswordTB.Text,
+                        Login = LoginTB.Text,
+                        idViewUser = usAddIdViewUser,
+                        YearBirth = (DateTime)YearBitrhtTB.DataContext,
+                        ResidentialAddress = AdresTB.Text,
+                        PlaceWork = WorkTB.Text,
+                        NumbrePhone = NumberPhoneTB.Text,
+                    };
+                    db.context.User.Add(user);
+                    db.context.SaveChanges();
+                }   
+            }
+            else
+            {
+                MessageBox.Show("Данные не введины");
+            }
+        }
+
+        private void Biblioteck_Click(object sender, RoutedEventArgs e)
+        {
+            usAddIdViewUser = 2;
+        }
+
+        private void Reader_Click(object sender, RoutedEventArgs e)
+        {
+            usAddIdViewUser = 3;
         }
     }
 }
