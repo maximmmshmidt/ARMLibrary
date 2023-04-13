61941a6305d682c8c8f873bf835822458be92f0f
maximmmshmidt <maximmm.shmidt@mail.ru>
2023-04-13 09:05:20 +0500

    фыв

--- ARMLibrary/Pages/PagesUser/Add/AddAutorPage.xaml.cs
+++ ARMLibrary/Pages/PagesUser/Add/AddAutorPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ARMLibrary.Models;
+using DocumentFormat.OpenXml.Drawing.Charts;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +10,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,15 +24,46 @@ namespace ARMLibrary.Pages.PagesUser.Add
     /// </summary>
     public partial class AddAutorPage : Page
     {
+        readonly Core db = new Core();
         public AddAutorPage()
         {
             InitializeComponent();
         }
-
-
+        
         private void Add(object sender, RoutedEventArgs e)
         {
-
+            if (LastNameTB != null && FirstNameTB != null && PatronicTB!=null && DateBirth != null && DateDeath != null )
+            {
+                if (DateDeath != null)
+                {
+                    Author author = new Author()
+                    {
+                        FirstName = FirstNameTB.Text,
+                        LastName = LastNameTB.Text,
+                        Patronymic = PatronicTB.Text,
+                        YearBirth = (DateTime)DateBirth.SelectedDate,
+                        YearDeath = (DateTime)DateDeath.SelectedDate,
+                    };
+                    db.context.Author.Add(author);
+                }
+                else
+                {
+                    Author author = new Author()
+                    {
+                        FirstName = FirstNameTB.Text,
+                        LastName = LastNameTB.Text,
+                        Patronymic = PatronicTB.Text,
+                        YearBirth = (DateTime)DateBirth.SelectedDate,
+                    };
+                    db.context.Author.Add(author);
+                }
+                
+                db.context.SaveChanges();
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Заполните все данные!!!");
+            }
         }
     }
 }
--- ARMLibrary/Pages/PagesUser/Add/AddBookPage.xaml.cs
+++ ARMLibrary/Pages/PagesUser/Add/AddBookPage.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
+﻿using ARMLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace ARMLibrary.Pages.PagesUser.Add
 {
@@ -20,12 +13,50 @@ namespace ARMLibrary.Pages.PagesUser.Add
     /// </summary>
     public partial class AddBookPage : Page
     {
+        readonly Core db = new Core();
         public AddBookPage()
         {
             InitializeComponent();
+            var author = new List<Author>();
+            author.AddRange(db.context.Author.ToList());
+
+            AutorTB.ItemsSource = author;
+            AutorTB.SelectedValuePath = "idAuthor";
+
+
+            var arrGroups = new List<Genre>();
+            arrGroups.AddRange(db.context.Genre.ToList());
+
+            IdJanreTB.ItemsSource = arrGroups;
+            IdJanreTB.DisplayMemberPath = "NameGenre";
+            IdJanreTB.SelectedValuePath = "idGenre";
+
         }
 
         private void Add(object sender, RoutedEventArgs e)
+        {
+            if (true)
+            {
+                var num = Convert.ToInt16(NumberPage.Text);
+                Book book = new Book()
+                {
+                    NameBook = NameBookTB.Text,
+                    idAuthor = AutorTB.Items.Count,
+                    idGenre = IdJanreTB.Items.Count,
+                    PublishingHouse = PublicationHouseTB.Text,
+                    PlacePublication = PlacePublTB.Text,
+                    YearPublication = YearPublTB.Text,
+                    NumberPages = num,
+                    ISBN = IsbnTB.Text,
+                    BBK = BbkTB.Text,
+                    Description = DescripTB.Text
+                };
+                db.context.Book.Add(book);
+                db.context.SaveChanges();
+            }
+        }
+
+        private void IdJanreTB_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
