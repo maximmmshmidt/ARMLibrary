f30d92199f38a4a84312cb5106e20e4ede0ae03a
maximmmshmidt <maximmm.shmidt@mail.ru>
2023-04-19 23:40:48 +0500

    asd

--- ARMLibrary/Pages/PagesUser/BookPageList.xaml.cs
+++ ARMLibrary/Pages/PagesUser/BookPageList.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ARMLibrary.Pages.PagesUser
 {
@@ -47,9 +48,64 @@ namespace ARMLibrary.Pages.PagesUser
                 ImageBook.Source = new BitmapImage( new Uri(book.ImageBook.ToString())); 
             }
         }
-        private void ExcelBTClick(object sender, RoutedEventArgs e)
+        private void ExcelBTClick(object sender, RoutedEventArgs e)   
         {
-            
+            /*создаем файл Excel*/
+
+            var aplication = new Excel.Application
+            {
+                Visible = true,
+
+                /*количество листов*/
+
+                SheetsInNewWorkbook = 1
+            };
+
+            /*добавляем рабочую книгу*/
+
+            Excel.Workbook workbook = aplication.Workbooks.Add(Type.Missing);
+
+            /*создаем лист*/
+
+            Excel.Worksheet worksheet = workbook.ActiveSheet;
+
+            worksheet.Name = "Student"; //имя листа нужно вводить латинскими буквами
+
+            /*заголовки вывод в Excel (в первую строку)*/
+
+            worksheet.Cells[1][1] = "Дата выдачи";
+            worksheet.Cells[2][1] = "Срок возварта";
+            worksheet.Cells[3][1] = "Отдел фонда";
+            worksheet.Cells[4][1] = "Автор";
+            worksheet.Cells[5][1] = "Название книги";
+            worksheet.Cells[6][1] = "Подпись Чит";
+            worksheet.Cells[7][1] = "Подпись Библ";
+            worksheet.Cells[8][1] = "Примечаниея";
+
+            worksheet.Cells[8][1].font.bold = true;
+            worksheet.Cells[7][1].font.bold = true;
+            worksheet.Cells[6][1].font.bold = true;
+            worksheet.Cells[5][1].font.bold = true;
+            worksheet.Cells[4][1].font.bold = true;
+            worksheet.Cells[3][1].font.bold = true;
+            worksheet.Cells[2][1].font.bold = true;
+            worksheet.Cells[1][1].font.bold = true;
+
+            /*вывод данных из массива в Excel*/
+
+            worksheet.Cells[1][2] = bok.NameBook;
+            worksheet.Cells[2][2] = bok.NameBook;
+            worksheet.Cells[3][2] = bok.NameBook;
+            worksheet.Columns.AutoFit();
+
+            //foreach (var item in bok)
+            //{
+            //    if (true)
+            //    {
+                    
+            //    }
+            //}
         }
+
     }
 }
