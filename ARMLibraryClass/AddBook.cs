5d36a885735815dc5801e515840ba68791705d37
maximmmshmidt <maximmm.shmidt@mail.ru>
2023-05-06 22:59:37 +0500

    asd

--- ARMLibraryClass/AddBook.cs
+++ ARMLibraryClass/AddBook.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ARMLibraryClass
 {
     public class AddBook
     {
+        static Regex reg;
+        static Match match;
+
+        public static bool Reg_FIO(string text)
+        {
+            reg = new Regex(@"^[A-ЯЁ][а-яё]{2,}");
+            match = reg.Match(text);
+            if (match.Success)
+            {
+                return true;
+            }
+            else { return false; }
+        }
     }
 }
