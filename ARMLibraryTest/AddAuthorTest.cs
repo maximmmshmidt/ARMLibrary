b57da6861443182f1f9131497957b71542bb996a
maximmmshmidt <maximmm.shmidt@mail.ru>
2023-05-11 22:52:08 +0500

    asd

---
+++ ARMLibraryTest/AddAuthorTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ARMLibraryTest
+{
+    [TestClass]
+    public class AddAuthorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ARMLibraryTest/AddBookTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ARMLibraryTest
+{
+    [TestClass]
+    public class AddBookTst
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
