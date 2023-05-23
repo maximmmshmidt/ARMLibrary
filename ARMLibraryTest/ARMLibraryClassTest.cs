571455a802f23d5d2db9d3e9131d89087d1d895c
maximmmshmidt <maximmm.shmidt@mail.ru>
2023-05-23 23:32:52 +0500

    asd

--- ARMLibraryTest/ARMLibraryClassTest.cs
+++ ARMLibraryTest/ARMLibraryClassTest.cs
@@ -1,26 +1,50 @@
 ﻿using ARMLibrary.Models;
+using ARMLibraryClass;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 namespace ARMLibraryTest
 {
     [TestClass]
     public class ARMLibraryClassTest
     {
-        readonly Core db = new Core();
+        /// <summary>
+        /// Проверка на автора
+        /// </summary>
         [TestMethod]
-        public void TestSuccessfulAddDepartment()
+        public void Author_FIO_Null()
         {
-
-            // Arrange
-            Core departments = new Core()
-            {
-                Name = "Тест",
-            };
-            // Act
-            bool result = departamentController.AddDepartment(departments);
-            // Assert
-            Assert.IsTrue(result);
+            //Arrange
+            string FIO = "";
+            //Act
+            bool actual = AddAuthor.Reg_FIO(FIO);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        /// <summary>
+        /// Проверка на книгу
+        /// </summary>
+        [TestMethod]
+        public void Book_Name()
+        {
+            //Arrange
+            string name = "";
+            //Act
+            bool actual = AddBook.Reg_NameBook(name);
+            //Assert
+            Assert.IsFalse(actual);
+        }
+        /// <summary>
+        /// Проверка на Пользователя
+        /// </summary>
+        [TestMethod]
+        public void User()
+        {
+            //Arrange
+            string FIO = "";
+            //Act
+            bool actual = AddUser.Reg_FIO(FIO);
+            //Assert
+            Assert.IsFalse(actual);
         }
     }
 }
